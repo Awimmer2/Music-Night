3012552d4ea553e8c7778e877daac909f821073a
Awimmer2 <a.wimmer2@students.htl-leonding.ac.at>
2025-05-08 11:50:01 +0200

    Added website footer to all pages & light-mode to some

--- Genre-Page/script.js
+++ Genre-Page/script.js
@@ -1,22 +1,24 @@
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
-
 if (localStorage.getItem('theme') === 'dark') {
   body.classList.add('dark-mode');
+  themeToggle.textContent = 'Light Mode';
 } else {
   body.classList.add('light-mode');
+  themeToggle.textContent = 'Dark Mode';
 }
-
 
 themeToggle.addEventListener('click', () => {
   if (body.classList.contains('dark-mode')) {
     body.classList.remove('dark-mode');
     body.classList.add('light-mode');
     localStorage.setItem('theme', 'light');
+    themeToggle.textContent = 'Dark Mode';
   } else {
     body.classList.remove('light-mode');
     body.classList.add('dark-mode');
     localStorage.setItem('theme', 'dark');
+    themeToggle.textContent = 'Light Mode';
   }
 });
