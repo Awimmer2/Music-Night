1a289120cbccb1fe4bb20bcc4ec55c6912e33f77
Awimmer2 <a.wimmer2@students.htl-leonding.ac.at>
2025-05-08 11:25:31 +0200

    Fixed light-mode dark-mode button

--- Pop-Page/script.js
+++ Pop-Page/script.js
@@ -1,17 +1,24 @@
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 
-
 if (localStorage.getItem('theme') === 'dark') {
   body.classList.add('dark-mode');
+  themeToggle.textContent = 'Light Mode';
+} else {
+  body.classList.add('light-mode');
+  themeToggle.textContent = 'Dark Mode';
 }
 
 themeToggle.addEventListener('click', () => {
-  body.classList.toggle('dark-mode');
-  
   if (body.classList.contains('dark-mode')) {
-    localStorage.setItem('theme', 'dark');
-  } else {
+    body.classList.remove('dark-mode');
+    body.classList.add('light-mode');
     localStorage.setItem('theme', 'light');
+    themeToggle.textContent = 'Dark Mode';
+  } else {
+    body.classList.remove('light-mode');
+    body.classList.add('dark-mode');
+    localStorage.setItem('theme', 'dark');
+    themeToggle.textContent = 'Light Mode';
   }
 });
